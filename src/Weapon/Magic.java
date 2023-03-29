0e2d7380305cbb7173487383232f9fcc8246a3a2
Theepob <theepob15112@gmail.com>
2023-03-29 23:08:45 +0700

    fix magic

--- src/Weapon/Magic.java
+++ src/Weapon/Magic.java
@@ -1,5 +1,9 @@
 package Weapon;
 
-public class Magic {
+import Skill.Skill;
 
+public class Magic extends BaseWeapon {
+	public Magic(int attack, int speed, Skill skill) {
+		super(attack, speed, skill);
+	}
 }

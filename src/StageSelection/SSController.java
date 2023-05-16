9941a996fb46e956126792770dd8232649e30eb5
Theepob <theepob15112@gmail.com>
2023-05-16 12:17:23 +0700

    add select Stage

--- src/StageSelection/SSController.java
+++ src/StageSelection/SSController.java
@@ -5,6 +5,7 @@ import application.StartPage;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -17,68 +18,67 @@ public class SSController{
 	@FXML
 	private Button slideleft;
 	@FXML
-	private Rectangle right;
+	private ImageView left = new ImageView(new Image((String) (ClassLoader.getSystemResource("Stage 2.png").toString())));
 	@FXML
-	private Rectangle left;
+	private ImageView right = new ImageView(new Image((String) (ClassLoader.getSystemResource("Stage 3.png").toString())));
 	@FXML
-	private Rectangle center;
+	private ImageView  center =new ImageView( new Image((String) (ClassLoader.getSystemResource("theNightmareExamRoom.png").toString())));
 	@FXML
 	private Text numStage;
 	@FXML
 	private ImageView Home;
 	@FXML
 	private Button Confirm;
-	
+	private Image imgtemp;
 	private int SelectedStage = 1;
 	
 	public void slideleft() {
 		if(SelectedStage == 1 ) {
-			center.setFill(Color.BLUE);
-			right.setFill(Color.RED);
-			left.setFill(Color.GREEN);
+			left.setImage(new Image((String) (ClassLoader.getSystemResource("theNightmareExamRoom.png").toString())));
+			center.setImage(new Image((String) (ClassLoader.getSystemResource("Stage 3.png").toString())));
+	        right.setImage(new Image((String) (ClassLoader.getSystemResource("Stage 2.png").toString())));
 			numStage.setText("NameStage 2");
 			SelectedStage=2;
 		}
 		else if(SelectedStage == 2) {
-			center.setFill(Color.GREEN);
-			right.setFill(Color.BLUE);
-			left.setFill(Color.RED);
+			left.setImage(new Image((String) (ClassLoader.getSystemResource("Stage 3.png").toString())));
+			center.setImage(new Image((String) (ClassLoader.getSystemResource("Stage 2.png").toString())));
+			right.setImage(new Image((String) (ClassLoader.getSystemResource("theNightmareExamRoom.png").toString())));
 			numStage.setText("NameStage 3");
 			SelectedStage=3;
 		}
 		else {
-			center.setFill(Color.RED);
-			right.setFill(Color.GREEN);
-			left.setFill(Color.BLUE);
-			numStage.setText("NameStage 1");
+			left.setImage(new Image((String) (ClassLoader.getSystemResource("Stage 2.png").toString())));
+			center.setImage(new Image((String) (ClassLoader.getSystemResource("theNightmareExamRoom.png").toString())));
+			right.setImage(new Image((String) (ClassLoader.getSystemResource("Stage 3.png").toString())));
+			numStage.setText("the Nightmare Exam Room");
 			SelectedStage=1;
 			
 		}
 	}
 	public void slideright() {
 		if(SelectedStage == 1 ) {
-			center.setFill(Color.GREEN);
-			right.setFill(Color.BLUE);
-			left.setFill(Color.RED);
+			left.setImage(new Image((String) (ClassLoader.getSystemResource("Stage 3.png").toString())));
+			center.setImage(new Image((String) (ClassLoader.getSystemResource("Stage 2.png").toString())));
+			right.setImage(new Image((String) (ClassLoader.getSystemResource("theNightmareExamRoom.png").toString())));
 			numStage.setText("NameStage 3");
 			SelectedStage=3;
 		}
 		else if(SelectedStage == 3) {
-			center.setFill(Color.BLUE);
-			right.setFill(Color.RED);
-			left.setFill(Color.GREEN);
+			left.setImage(new Image((String) (ClassLoader.getSystemResource("theNightmareExamRoom.png").toString())));
+			center.setImage(new Image((String) (ClassLoader.getSystemResource("Stage 3.png").toString())));
+	        right.setImage(new Image((String) (ClassLoader.getSystemResource("Stage 2.png").toString())));
 			numStage.setText("NameStage 2");
 			SelectedStage=2;
 		}
 		else {
-			center.setFill(Color.RED);
-			right.setFill(Color.GREEN);
-			left.setFill(Color.BLUE);
-			numStage.setText("NameStage 1");
+			left.setImage(new Image((String) (ClassLoader.getSystemResource("Stage 2.png").toString())));
+			center.setImage(new Image((String) (ClassLoader.getSystemResource("theNightmareExamRoom.png").toString())));
+			right.setImage(new Image((String) (ClassLoader.getSystemResource("Stage 3.png").toString())));
+			numStage.setText("the Nightmare Exam Room");
 			SelectedStage=1;
-			
 		}
-	}
+}
 	public void goHome() {
 		Stage thisStage = (Stage) Home.getScene().getWindow();
 		thisStage.setScene(StartPage.getStartPageScene());

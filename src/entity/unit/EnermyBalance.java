d0c94398decca0ceccff73ebe145b91f07957141
Theepob <theepob15112@gmail.com>
2023-05-21 14:45:50 +0700

    change enemy color

--- src/entity/unit/EnermyBalance.java
+++ src/entity/unit/EnermyBalance.java
@@ -5,7 +5,7 @@ import javafx.scene.paint.Color;
 public class EnermyBalance extends Enemy{
 
 	public EnermyBalance(Player p, int x, int y) {
-		super(p, x, y, 40,2, Color.BLACK,20,5);
+		super(p, x, y, 40,2, Color.BROWN,20,5);
 	}
 
 }
